60c4c2c8975f71a2970aeba1689cb9da99f24447
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-11-27 23:21:28 +0200

    Added more styling to the newsletter tab. Added unsubscribe view for the future newsletter unsubscribe option.

--- newsletters/forms.py
+++ newsletters/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from newsletters.models import SubscribersList
 
@@ -6,3 +7,6 @@ class SubscribeForm(ModelForm):
     class Meta:
         model = SubscribersList
         fields = ['subscriber_email']
+
+    subscriber_email = forms.CharField(widget=forms.EmailInput(attrs={'placeholder': 'Въведи своята електранна поща',
+                                                                      }))
--- newsletters/views.py
+++ newsletters/views.py
@@ -18,9 +18,19 @@ def newsletter(request):
 def subscribe(request):
     email_input = request.POST['subscriber_email']
     if SubscribersList.objects.filter(subscriber_email=email_input):
-        raise Exception("Email already subscribed!")
+        raise Exception("Електранната поща вече съществува в базата данни!")
     else:
         new_subscriber = SubscribersList(subscriber_email=email_input)
         new_subscriber.save()
+
     return HttpResponseRedirect(reverse('newsletter'))
 
+
+def unsubscribe(request):
+    if request.method == "DELETE":
+        email_input = request.POST['subscriber_email']
+        if SubscribersList.objects.filter(subscriber_email=email_input):
+            SubscribersList.objects.filter(subscriber_email=email_input).delete()
+        else:
+            raise Exception("Невалидна електранна поща!")
+        return HttpResponseRedirect(reverse('newsletter'))
