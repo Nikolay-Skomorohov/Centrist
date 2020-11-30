91d9c81c5d797145b087b37445748363a95d268e
Nikolay Skomorohov <nikolay_skomorohov@protonmail.com>
2020-11-30 23:14:37 +0200

    Added basic pagination for the home/newest page. No CSS.

--- common/views.py
+++ common/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
@@ -9,7 +10,12 @@ from articles.models import Article
 
 def index(request):
     template = loader.get_template('common/home.html')
-    context = {"articles_list": Article.objects.all, }
+    articles_list = Article.objects.order_by('id').reverse()
+    paginator = Paginator(articles_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {"articles_list": articles_list, "page_obj": page_obj}
     return HttpResponse(template.render(context, request))
 
 
